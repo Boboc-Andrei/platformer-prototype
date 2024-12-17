efe99b44b1248e4858f491d7b22c6dcf99ac02a0
Boboc-Andrei <boboc.andrei999@yahoo.com>
2024-12-17 20:29:03 +0200

    Improved placing of ledge grab colliders

---
+++ Assets/_Scripts/LedgeGrab.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public class LedgeGrab : MonoBehaviour
+{
+    public Vector2 StartOffset = new Vector2(.5f, -.75f);
+    public Vector2 EndOffset = new Vector2(-.5f, 1.125f);
+
+
+}
