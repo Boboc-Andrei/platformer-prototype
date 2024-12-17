66b5d72669900cd6ff2c4666dfc059f3630a3eef
Boboc-Andrei <boboc.andrei999@yahoo.com>
2024-12-17 02:57:31 +0200

    Bugfixing

--- Assets/Environment/Tilemaps/SuperTiled/8x8_placeholder13_full.tsx
+++ Assets/Environment/Tilemaps/SuperTiled/8x8_placeholder13_full.tsx
@@ -3,15 +3,15 @@
  <image source="8x8_placeholder13_full.png" width="40" height="24"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="8" y="7" rotation="270">
-    <polygon points="0,0 0,-1 -1,-1 -1,-8 7,-8 7,0"/>
+   <object id="1" x="7" y="8">
+    <polygon points="0,0 0,-1 1,-1 1,-8 -7,-8 -7,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="1">
-  <objectgroup draworder="index" id="2">
-   <object id="6" x="1" y="8">
-    <polygon points="0,0 0,-1 -1,-1 -1,-8 7,-8 7,0"/>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="-8.88178e-16" y="7" rotation="90">
+    <polygon points="0,0 0,-1 1,-1 1,-8 -7,-8 -7,0"/>
    </object>
   </objectgroup>
  </tile>
@@ -32,15 +32,15 @@
  </tile>
  <tile id="5">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="7" y="4.44089e-16" rotation="180">
-    <polygon points="0,0 0,-1 -1,-1 -1,-8 7,-8 7,0"/>
+   <object id="1" x="8" y="1" rotation="270">
+    <polygon points="0,0 0,-1 1,-1 1,-8 -7,-8 -7,0"/>
    </object>
   </objectgroup>
  </tile>
  <tile id="6">
   <objectgroup draworder="index" id="2">
-   <object id="2" x="4.44089e-16" y="1" rotation="90">
-    <polygon points="0,0 0,-1 -1,-1 -1,-8 7,-8 7,0"/>
+   <object id="1" x="1" y="0" rotation="180">
+    <polygon points="0,0 0,-1 1,-1 1,-8 -7,-8 -7,0"/>
    </object>
   </objectgroup>
  </tile>
@@ -61,7 +61,7 @@
  </tile>
  <tile id="12">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="8" height="7"/>
+   <object id="1" x="1" y="0" width="7" height="7"/>
   </objectgroup>
  </tile>
  <tile id="13">
@@ -71,7 +71,7 @@
  </tile>
  <tile id="14">
   <objectgroup draworder="index" id="2">
-   <object id="1" x="0" y="0" width="7" height="8"/>
+   <object id="1" x="0" y="0" width="7" height="7"/>
   </objectgroup>
  </tile>
 </tileset>
---
+++ Assets/Environment/Tilemaps/SuperTiled/8x8_simple_platform_upper.tsx
@@ -0,0 +1,24 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="8x8_simple_platform_upper" tilewidth="8" tileheight="8" tilecount="8" columns="4">
+ <image source="8x8_platform_upper.png" width="32" height="16"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="7" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="8" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="8" height="3"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="7" height="3"/>
+  </objectgroup>
+ </tile>
+</tileset>
