c3fcba85b3acc6de43963d5e4d316be92893eee8
Boboc-Andrei <boboc.andrei999@yahoo.com>
2024-12-17 02:08:13 +0200

    Redesigned tile hitbox

---
+++ Assets/Environment/Tilemaps/SuperTiled/8x8_placeholder13_full.tsx
@@ -0,0 +1,77 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="8x8_placeholder13_full" tilewidth="8" tileheight="8" tilecount="15" columns="5">
+ <image source="8x8_placeholder13_full.png" width="40" height="24"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="8" y="7" rotation="270">
+    <polygon points="0,0 0,-1 -1,-1 -1,-8 7,-8 7,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="6" x="1" y="8">
+    <polygon points="0,0 0,-1 -1,-1 -1,-8 7,-8 7,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="1" width="7" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="3">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="8" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="4">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="1" width="7" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="5">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="7" y="4.44089e-16" rotation="180">
+    <polygon points="0,0 0,-1 -1,-1 -1,-8 7,-8 7,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="6">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="4.44089e-16" y="1" rotation="90">
+    <polygon points="0,0 0,-1 -1,-1 -1,-8 7,-8 7,0"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="7">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="1" y="0" width="7" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="8">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="9">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="7" height="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="12">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="13">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="8" height="7"/>
+  </objectgroup>
+ </tile>
+ <tile id="14">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="0" width="7" height="8"/>
+  </objectgroup>
+ </tile>
+</tileset>
